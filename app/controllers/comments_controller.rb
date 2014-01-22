8da0405df3f50a9896ee47ca31bdf8ce2184b8c9
張旭 <zx1986@gmail.com>
2014-01-22 17:09:27 +0800

    新增 comments
    http_basic_authenticate_with 效果很像 Apache 的 Auth
    常用 rake routes 多看看，多使用 resources 在 routes.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  # http_basic_authenticate_with name: "larvata", password: "12345678", only: [:destroy]
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:commenter, :content))
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
-	def index
-		
-	end
-	
+  def index
+    # render text: 'hello there!'
+  end
+  
 end
